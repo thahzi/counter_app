3147ab47e3ced23ca1f89a68c641c39639297dca
thahzi <thahzeebmohamed@gmail.com>
2024-06-27 11:01:13 +0530

    june27

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div style={{backgroundColor:"white", height:'100vh'}} className="d-flex align-items-center justify-content-center">
+
+      <div style={{backgroundColor:"white", width:'500px'}} className="box d-flex align-items-center justify-content-center">
+
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
