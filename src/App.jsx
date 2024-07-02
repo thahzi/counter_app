f3a99c546b691e6a1fa6de4e6e7d63273afd5c6e
thahzi <thahzeebmohamed@gmail.com>
2024-07-02 09:42:56 +0530

    july2

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,26 @@ function App() {
 
   return (
     <>
-      <div style={{backgroundColor:"white", height:'100vh'}} className="d-flex align-items-center justify-content-center">
+      <div style={{backgroundColor:"black", height:'100vh'}} className="d-flex align-items-center justify-content-center">
 
-      <div style={{backgroundColor:"white", width:'500px'}} className="box d-flex align-items-center justify-content-center">
+        <div style={{backgroundColor:"white", width:'500px'}} className=" p-5 rounded">
+          <h1 className='text-info'>counter application</h1>
 
-      </div>
+          <h1 className='text-center my-5'style={{fontSize:"50px"}}>0</h1>
+
+          <div className="mt-5 d-flex justify-content-evenly">
+
+            <button className='btn btn-warning'>Decrement</button>
+            <button className='btn btn-danger'>reset</button>
+            <button className='btn btn-success'>increment</button>
+
+          </div>
+            <div className="d-flex mt-5">
+              <input type="text" placeholder='range'className='form-control'/>
+              <button className='btn btn-primary ms-3'>click</button>
+            </div>
+        </div>
+
       </div>
     </>
   )
